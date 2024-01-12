966a30946b968aedaf1e3f4dd5d23b12c0bf903f
Phil <phil@pheedloop.com>
2024-01-12 12:59:24 +0900

    reformat to org mode, add functions from wiki

--- README.md
+++ README.md
@@ -1,76 +1,63 @@
-# gptel-extensions
+* gptel-ext
 
-Extra functions for [gptel](https://github.com/karthink/gptel), a package that allows you to use [OpenAI's GPT](https://openai.com/) for text generation and completion directly from Emacs.
+Extra functions for [[https://github.com/karthink/gptel][gptel]], a package that allows you to use various LLM providers for text generation and completion directly from Emacs.
 
-## Installation
+** Functions
 
-1. Clone the repository to a local directory with:
+*** =gptel-ext-send-whole-buffer=
 
-   ```
-   git clone https://github.com/kamushadenes/gptel-extensions.el.git
-   ```
+Jump to the bottom of the buffer and then send the entire buffer.
 
-2. Add the following to your Emacs init file (usually `~/.emacs` or `~/.emacs.d/init.el`):
-
-   ```
-   (add-to-list 'load-path "path/to/gptel-extensions")
-   (require 'gptel-extensions)
-   ```
-
-   Replace `path/to/gptel-extensions` with the path where you cloned the repository.
-
-3. Restart Emacs, or evaluate the added code with `M-x eval-buffer` while in the init file.
-
-## Functions
-
-### `gptel-ext-send-whole-buffer`
-
-Send the whole buffer to ChatGPT.
-
-### `gptel-ext-ask-document`
+*** =gptel-ext-ask-document=
 
 Load the current buffer into a session so you can ask questions about it.
 
-### `gptel-ext-rewrite-and-replace`
+*** =gptel-ext-rewrite-and-replace=
 
-Rewrite the region or sentence at point and replace it with the response. Extracted from the [Wiki](https://github.com/karthink/gptel/wiki).
+Rewrite the region or sentence at point and replace it with the response. Extracted from the [[https://github.com/karthink/gptel/wiki][Wiki]].
 
-### `gptel-ext-refactor`
+*** =gptel-ext-quick=
+
+Accept a prompt in the minibuffer and pop up a window with the response. Extracted from the [[https://github.com/karthink/gptel/wiki][Wiki]].
+
+*** =gptel-ext-refactor=
 
 Refactor the region or sentence at point.
 
-## Variables
+** Variables
 
-### `gptel-ext-ask-document-prefix`
+*** =gptel-ext-ask-document-prefix=
 
 Prefix to use when asking questions about a document. Defaults to:
 
-```text
+#+BEGIN_EXAMPLE
 Your task is to answer questions about the following document. If you don't know the answer, reply with "I don't know"
 
 ###### DOCUMENT START ######
 
-```
+#+END_EXAMPLE
 
-### `gptel-ext-ask-document-suffix`
+*** =gptel-ext-ask-document-suffix=
 
 Suffix to use when asking questions about a document. Defaults to:
 
-```text
+#+BEGIN_EXAMPLE
 
 ###### DOCUMENT END ######
 
 ### Question: 
-```
+#+END_EXAMPLE
 
-### `gptel-ext-refactor-directive`
+*** =gptel-ext-refactor-directive=
 
 Directive to use when refactoring code. Defaults to:
 
-```text
+#+BEGIN_EXAMPLE
 You are a programmer. Refactor my code to improve readability. Reply only with the code.
-```
+#+END_EXAMPLE
 
-## Credits
+** Credits
 
-This merely extends the amazing [gptel](https://github.com/karthink/gptel/) by Karthik Chikmagalur, who performed all the hard work.
+This merely extends the amazing [[https://github.com/karthink/gptel/][gptel]] by Karthik Chikmagalur, who performed all the hard work.
+
+This package is a fork of [[https://github.com/kamushadenes/gptel-extensions.el][gptel-extensions]] by [[https://github.com/kamushadenes][kamushadenes]].
--- gptel-ext.el
+++ gptel-ext.el
@@ -1,7 +1,7 @@
-;;; gptel-extensions.el --- Extra functions for gptel
+;;; gptel-ext.el --- Extra functions for gptel
 
-;; Author: Henrique Goncalves <kamus@hadenes.io>
-;; URL: https://github.com/kamushadenes
+;; Author: Phil Isaac <isaac.phil@gmail.com>
+;; URL: https://github.com/isaacphi/gptel-ext
 ;; Version: 0.0.1
 
 ;;; Commentary:
@@ -25,9 +25,12 @@
 (defvar gptel-ext-refactor-directive "You are a programmer. Refactor my code to improve readability. Reply only with the code."
   "Directive to use when refactoring code.")
 
+(defvar gptel-ext-rewrite-and-replace "You are a programmer. Re-write this code."
+  "Directive to use when replacing code.")
+
 ;;;###autoload
 (defun gptel-ext-send-whole-buffer ()
-  "Send the whole buffer to ChatGPT."
+  "Send the whole buffer to Chat."
   (interactive)
   (mark-whole-buffer)
   (goto-char (point-max))
@@ -49,6 +52,29 @@
 ;; extracted from the wiki
 ;;
 ;;;###autoload
+(defvar gptel-ext-quick--history nil)
+(defun gptel-ext-quick (prompt)
+  (interactive (list (read-string "Ask: " nil gptel-quick--history)))
+  (when (string= prompt "") (user-error "A prompt is required."))
+  (gptel-request
+   prompt
+   :callback
+   (lambda (response info)
+     (if (not response)
+         (message "gptel-ext-quick failed with message: %s" (plist-get info :status))
+       (with-current-buffer (get-buffer-create "*gptel-quick*")
+         (let ((inhibit-read-only t))
+           (erase-buffer)
+           (insert response))
+         (special-mode)
+         (display-buffer (current-buffer)
+                         `((display-buffer-in-side-window)
+                           (side . bottom)
+                           (window-height . ,#'fit-window-to-buffer))))))))
+
+;; extracted from the wiki
+;;
+;;;###autoload
 (defun gptel-ext-rewrite-and-replace (bounds &optional directive)
   "Rewrite the region or sentence at point and replace it with the response."
   (interactive
@@ -59,18 +85,17 @@
       (list (bounds-of-thing-at-point 'sentence)))
      (t (cons (line-beginning-position) (line-end-position))))
     (and current-prefix-arg
-         (read-string "ChatGPT Directive: "
-                      "You are a prose editor. Rewrite my prompt more professionally."))))
+         (read-string gptel-ext-rewrite-and-replace))))
   (gptel-request
    (buffer-substring-no-properties (car bounds) (cdr bounds)) ;the prompt
-   :system (or directive "You are a prose editor. Rewrite my prompt more professionally.")
+   :system (or directive gptel-ext-rewrite-and-replace))
    :buffer (current-buffer)
    :context (cons (set-marker (make-marker) (car bounds))
                   (set-marker (make-marker) (cdr bounds)))
    :callback
    (lambda (response info)
      (if (not response)
-         (message "ChatGPT response failed with: %s" (plist-get info :status))
+         (message "Chat response failed with: %s" (plist-get info :status))
        (let* ((bounds (plist-get info :context))
               (beg (car bounds))
               (end (cdr bounds))
@@ -82,7 +107,7 @@
              (insert response)
              (set-marker beg nil)
              (set-marker end nil)
-             (message "Rewrote line. Original line saved to kill-ring."))))))))
+             (message "Rewrote line. Original line saved to kill-ring.")))))))
 
 ;;;###autoload
 (defun gptel-ext-refactor (bounds)
@@ -97,6 +122,35 @@
   (message "Refactoring...")
   (gptel-ext-rewrite-and-replace bounds gptel-ext-refactor-directive))
 
-(provide 'gptel-extensions)
+(cl-defun gptel-ext-clean-up-gptel-refactored-code ()
+  "Clean up the code responses for refactored code in the current buffer.
+Current buffer is guaranteed to be the response buffer."
+  (when gptel-mode ; Don't want this to happen in the dedicated buffer.
+    (cl-return-from my/clean-up-gptel-refactored-code))
+  (save-excursion
+    (let* ((res-beg (point-min))
+           (res-end nil)
+           (contents nil))
+      (unless (get-text-property res-beg 'gptel)
+        (setq res-beg (next-single-property-change res-beg 'gptel)))
+      (while res-beg
+        (setq res-end (next-single-property-change res-beg 'gptel))
+        (unless res-end
+          (setq res-end (point-max)))
+        (setq contents (buffer-substring-no-properties res-beg
+                                                       res-end))
+        (setq contents (replace-regexp-in-string "\n*``.*\n*"
+                                                 ""
+                                                 contents))
+        (delete-region res-beg res-end)
+        (goto-char res-beg)
+        (insert contents)
+        (setq res-end (point))
+        ;; Indent the code to match the buffer indentation if it's messed up.
+        (indent-region res-beg res-end)
+        (pulse-momentary-highlight-region res-beg res-end)
+        (setq res-beg (next-single-property-change res-beg 'gptel))))))
 
-;;; gptel-extensions.el ends here
+(add-hook 'gptel-post-response-hook #'gptel-ext-clean-up-gptel-refactored-code)
+
+(provide 'gptel-extensions)
